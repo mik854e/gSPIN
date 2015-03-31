93807369f62e9658589d514f4e43f39c1b362626
Michael Saltzman <mik854@gmail.com>
2015-03-30 23:53:27 -0400

    Added PowerAnnotation class.

---
+++ SPIN_Backend_Server/src/PowerAnnotation.java
@@ -0,0 +1,12 @@
+class PowerAnnotation {
+
+	public String type;
+	public String superior;
+	public String subordinate;
+	
+	public PowerAnnotation(String type, String superior, String subordinate) {
+		this.type = type;
+		this.superior = superior;
+		this.subordinate = subordinate;
+	}
+}
